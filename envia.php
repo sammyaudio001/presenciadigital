32fc250796fb2ef69a1e630595d8a1a23a9be5cd
sammyaudio001 <diazsamuel001@yahoo.com>
2022-02-12 16:15:51 -0400

    Mi web

---
+++ envia.php
@@ -0,0 +1,27 @@
+﻿<?php
+$remitente = $_POST['correo'];
+$destinatario = 'info@presenciadigital.org'; 
+$asunto = 'consulta'; 
+if (!$_POST){
+?>
+
+<?php
+}else{
+	 
+    $cuerpo = "nombre: " . $_POST["nombre"] . "\r\n"; 
+    $cuerpo .= "correo: " . $_POST["correo"] . "\r\n";
+	$cuerpo .= "consulta: " . $_POST["consulta"] . "\r\n";
+	
+
+    $headers  = "MIME-Version: 1.0\n";
+    $headers .= "Content-type: text/plain; charset=utf-8\n";
+    $headers .= "X-Priority: 3\n";
+    $headers .= "X-MSMail-Priority: Normal\n";
+    $headers .= "X-Mailer: php\n";
+    $headers .= "From: \"".$_POST['nombre']." ".$_POST['correo']."\" <".$remitente.">\n";
+
+    mail($destinatario, $asunto, $cuerpo, $headers);
+    
+    include 'recibe.php'; 
+}
+?>
---
+++ recibe.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="es">
+  <head>
+  <meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Diseño de paginas web en Buenos Aires, Administracion de paginas en Wordpress" />
+<meta name="keywords" content="diseño de paginas web en buenos aires, paginas web en buenos aires, wordpress, diseño de wordpress en buenos aires, woocommerce,diseño de tiendas online buenos aires, administracion de wordpress buenos aires, diseño de pagina html en buenos aires, paginas responsive en buenos aires, paginas para dispositivos moviles, paginas web para celulares" />
+    <title>Presencia Digital</title>
+
+    
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+<link rel="stylesheet" href="style.css">
+    <title>Presencia Digital</title>
+  </head>
+  <body>
+  <section id="logo">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 text-center">
+            <img src="./img/logonuevo.png" class="img-fluid">
+          </div>
+        </div>
+      </div>
+    </section>
+    <h3  class="text-center" style="color:white;">Estamos a un paso de comenzar a crecer juntos!</h3>
+    <br>
+    <h1 style="color:cornsilk" class="text-center">Hemos recibido tu requerimiento!</h1>
+   
+    <button type="button" class="btn btn-primary"><a href="index.html" style="color: cornsilk; padding:20px" >Volver</a></button>
+
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+  </body>
+</html>
